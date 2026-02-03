0378bdc09b8123f92c559deb02adebff493ec8d2
leanr264 <leandroracedo2604@gmail.com>
2026-02-03 20:58:49 -0300

    Se actualizo la creacion de cuentas de dinero y el redireccionamiento en vista de registro

--- public/register/register.js
+++ public/register/register.js
@@ -24,18 +24,12 @@ btnRegister.addEventListener("click", async (e) => {
         password: password
       });
       console.log(response.data);
-      if (response.data.jwt) {
+
+      if (response) {
          alert("Registro exitoso. Revisa tu correo para verificar tu cuenta antes de iniciar sesión.");
           window.open("http://localhost:3000/", "_self");
-        
-        /*sessionStorage.setItem("token", response.data.jwt);
-        sessionStorage.setItem("userId", response.data.userId);
-        sessionStorage.setItem("firstName", response.data.firstName);
-        sessionStorage.setItem("lastName", response.data.lastName);
-        sessionStorage.setItem("email", response.data.userEmail);
-        console.log(sessionStorage.getItem("token"));
-        window.open("http://localhost:3000/home", "_self");*/
       }
+      
     } catch (error) {
       console.log(error);
       alert(error.response?.data?.message || "Hubo un error al registrar el usuario");
