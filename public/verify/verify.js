d2c163173220154361ae086de6eef63b87f117fa
leanr264 <leandroracedo2604@gmail.com>
2026-02-18 19:51:10 -0300

    Se agregaron estilos para la verificación de las cuentas

--- public/verify/verify.js
+++ public/verify/verify.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  const messageElement = document.querySelector(".info-container p");
   console.log("verify.js cargado");
 
   try {
@@ -22,8 +23,20 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     console.log("Respuesta backend:", response.data);
 
+    messageElement.innerHTML = `
+        Su cuenta se verificó correctamente. Si desea iniciar sesión, diríjase a este link:
+        <a href="http://localhost:3000/">Iniciar sesión</a>
+    `;
+
   } catch (error) {
     console.error("Error verificando cuenta:", error);
+
+    messageElement.innerHTML = `
+        El enlace de verificación es inválido o ha expirado. 
+    `;
+
+    messageElement.style.color = "red"; 
+    messageElement.classList.add("text-danger");
   }
 });
 
